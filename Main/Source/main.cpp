7fd90bd1d9ce0b708fbdb10c29f351831f2f3b6f
Maxim Esikov <maksim91212191@gmail.com>
2020-12-27 23:39:53 +0300

    [+] #-: Init

---
+++ Main/Source/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdexcept>
+#include <iostream>
+#include <stdint.h>
+#include <time.h>
+#include <math.h>
+#include <wiringPiI2C.h>
+#include <wiringPi.h>
+#include <station/BME280/bme280.h>
+
+int main() {
+    std::cout << "Main" << std::endl;
+    return 0;
+}
