352a9366b4bd78c05e9dd80a2c4e902107bad0dc
Maxim Esikov <maksim91212191@gmail.com>
2021-01-23 15:26:43 +0300

    [+] TCA added

--- Main/Source/main.cpp
+++ Main/Source/main.cpp
@@ -10,58 +10,60 @@
 #include <bme/bme280.h>
 
 int main() {
-    char* device0 = "/dev/i2c-0";
+    char* device0 = "/dev/i2c-1";
+    int devIdTCA = 0x70;
 
-    int devId0 = BME280_I2C_ADDRESS1;
+    try {
+        int fdTCA = wiringPiI2CSetup(devIdTCA);
+        wiringPiI2CWrite(fdTCA, 1<<2);
 
-  try {
+        int devId0 = BME280_I2C_ADDRESS1;
+        BME280* bme2800 = new BME280(device0, devId0);
+        int fd0 = bme2800->init();
+        bme2800->reset();
 
-    BME280* bme2800 = new BME280(device0, devId0);
-    int fd0 = bme2800->init();
-    bme2800->reset();
+        if (fd0 < 0) {
+            printf("Device not found");
+            return -1;
+        }
 
-    if (fd0 < 0) {
-      printf("Device not found");
-      return -1;
+        printf("fd       : 0x%02x\n", fd0);
+        printf("chip id  : 0x%02x\n", bme2800->getChipId());
+        printf("chip ver : 0x%02x\n", bme2800->getChipVersion());
+
+        bme2800->reset();
+        bme2800->setPowerMode(BME280_NORMAL_MODE);
+        bme2800->setTemperatureOversampling(BME280_ULTRAHIGHRESOLUTION_OVERSAMP_TEMPERATURE);
+        bme2800->setPressureOversampling(BME280_ULTRAHIGHRESOLUTION_OVERSAMP_PRESSURE);
+        bme2800->setHumidityOversampling(BME280_ULTRAHIGHRESOLUTION_OVERSAMP_HUMIDITY);
+        bme2800->setIrrFilter(BME280_FILTER_COEFF_16);
+        bme2800->setStandbyTime(BME280_STANDBY_TIME_250_MS);
+
+        printf("---------------\n");
+        printf("pw mode  : 0x%02x\n", bme2800->getPowerMode());
+        printf("osrs_p   : 0x%02x\n", bme2800->getPressureOversampling());
+        printf("osrs_t   : 0x%02x\n", bme2800->getTemperatureOversampling());
+        printf("osrs_h   : 0x%02x\n", bme2800->getHumidityOversampling());
+        printf("---------------\n");
+        printf("filter   : 0x%02x\n", bme2800->getIrrFilter());
+        printf("t_sb     : 0x%02x\n", bme2800->getStandbyTime());
+        printf("---------------\n");
+        printf("spi3w sts: 0x%02x\n", bme2800->getSpi3w());
+        printf("measuring: 0x%02x\n", bme2800->getMeasuringStatus());
+        printf("im_update: 0x%02x\n", bme2800->getImUpdateStatus());
+        printf("---------------\n");
+
+        while (1) {
+            delay(1000);
+            BMP280Data * bme2800Data = bme2800->getBMP280Data();
+            printf("pressure   : %.2f \tmm Hg\n", bme2800Data->getPressure() / 1.3332239);
+            printf("humidity   : %.2f \t%c\n", bme2800Data->getHumidity(), '%');
+            printf("temperature: %.2f \t°C\n", bme2800Data->getTemperature());
+            printf("altitude   : %.2f \tm\n\n", bme2800Data->getAltitude());
+        }
+        delete bme2800;
+    } catch (std::exception & e) {
+        printf("%s\n", e.what());
     }
-
-    printf("fd       : 0x%02x\n", fd0);
-    printf("chip id  : 0x%02x\n", bme2800->getChipId());
-    printf("chip ver : 0x%02x\n", bme2800->getChipVersion());
-
-    bme2800->reset();
-    bme2800->setPowerMode(BME280_NORMAL_MODE);
-    bme2800->setTemperatureOversampling(BME280_ULTRAHIGHRESOLUTION_OVERSAMP_TEMPERATURE);
-    bme2800->setPressureOversampling(BME280_ULTRAHIGHRESOLUTION_OVERSAMP_PRESSURE);
-    bme2800->setHumidityOversampling(BME280_ULTRAHIGHRESOLUTION_OVERSAMP_HUMIDITY);
-    bme2800->setIrrFilter(BME280_FILTER_COEFF_16);
-    bme2800->setStandbyTime(BME280_STANDBY_TIME_250_MS);
-
-    printf("---------------\n");
-    printf("pw mode  : 0x%02x\n", bme2800->getPowerMode());
-    printf("osrs_p   : 0x%02x\n", bme2800->getPressureOversampling());
-    printf("osrs_t   : 0x%02x\n", bme2800->getTemperatureOversampling());
-    printf("osrs_h   : 0x%02x\n", bme2800->getHumidityOversampling());
-    printf("---------------\n");
-    printf("filter   : 0x%02x\n", bme2800->getIrrFilter());
-    printf("t_sb     : 0x%02x\n", bme2800->getStandbyTime());
-    printf("---------------\n");
-    printf("spi3w sts: 0x%02x\n", bme2800->getSpi3w());
-    printf("measuring: 0x%02x\n", bme2800->getMeasuringStatus());
-    printf("im_update: 0x%02x\n", bme2800->getImUpdateStatus());
-    printf("---------------\n");
-
-    while (1) {
-      delay(1000);
-      BMP280Data * bme2800Data = bme2800->getBMP280Data();
-      printf("pressure   : %.2f \tmm Hg\n", bme2800Data->getPressure() / 1.3332239);
-      printf("humidity   : %.2f \t%c\n", bme2800Data->getHumidity(), '%');
-      printf("temperature: %.2f \t°C\n", bme2800Data->getTemperature());
-      printf("altitude   : %.2f \tm\n\n", bme2800Data->getAltitude());
-    }
-    delete bme2800;
-  } catch (std::exception & e) {
-    printf("%s\n", e.what());
-  }
-  return 0;
+    return 0;
 }
